1cdd46c708e43deda9b7b5e398009fec04b7f496
tamj <104643052+nagbabalik@users.noreply.github.com>
2024-05-07 09:48:53 +0800

    Add files via upload

---
+++ music/js/music-list.js
@@ -0,0 +1,108 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+
+
+  {
+    name: "Emilyn-Antonio",
+    artist: "Marikit sa dilim",
+    img: "Emilyn-Antonio",
+    src: "Emilyn-Antonio"
+  },
+  {
+    name: "Lovely-Jethro",
+    artist: "lsd x medina",
+    img: "Lovely-Jethro",
+    src: "Lovely-Jethro"
+  },
+  {
+    name: "RuthAnne-Richard",
+    artist: "Pantropiko",
+    img: "RuthAnne-Richard",
+    src: "RuthAnne-Richard"
+  },
+  {
+    name: "Patricia Lorraine-John Lord",
+    artist: "Kedong Kedong",
+    img: "patty-1",
+    src: "patty-1"
+  },
+  {
+    name: "Archelle Israelah – Johancen",
+    artist: "Kung-fu",
+    img: "ella-2",
+    src: "ella-2"
+  },
+  {
+    name: "Nicole-Reynold",
+    artist: "Daleng-dale",
+    img: "Nicole-Reynold",
+    src: "Nicole-Reynold"
+  },
+  {
+    name: "Joanna - Christian",
+    artist: "% Little Monkeys",
+    img: "joanna-4",
+    src: "joanna-4"
+  },
+  {
+    name: "Allysa - Brian",
+    artist: "Whoop",
+    img: "Allysa-Brian",
+    src: "Allysa-Brian"
+  },
+  {
+    name: "Jinky - Skycraper",
+    artist: "Yo yo yo",
+    img: "jinky-6",
+    src: "jinky-6"
+  },
+  {
+    name: "Angie - Glen",
+    artist: "Selos",
+    img: "angie-7",
+    src: "angie-7"
+  },
+  {
+    name: "Rhea-Oliver",
+    artist: "Ride",
+    img: "Rhea-Oliver",
+    src: "Rhea-Oliver"
+  },
+  {
+    name: "Kamille - Joseph",
+    artist: "Paro Paro G",
+    img: "Kamille-9",
+    src: "Kamille-9"
+  },
+   {
+    name: "Clarisse - John Ralph",
+    artist: "Tahong ni Karla",
+    img: "clarisse-10",
+    src: "clarisse-10"
+  },
+  {
+    name: "Jennie Rose-Kenneth",
+    artist: "Salamin",
+    img: "Jennie Rose-Kenneth",
+    src: "Jennie Rose-Kenneth"
+  },
+
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
